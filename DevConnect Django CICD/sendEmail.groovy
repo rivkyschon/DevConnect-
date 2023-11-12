0bbc2c5f69a14e21053d144acef8c66f96d63cfc
rivkyschon <113901370+rivkyschon@users.noreply.github.com>
2023-11-12 02:37:05 +0200

    Create sendEmail.groovy

---
+++ DevConnect Django CICD/sendEmail.groovy
@@ -0,0 +1,33 @@
+// def sendEmailNotification(buildResult) {
+//     emailext subject: "Build ${buildResult}: ${env.JOB_NAME} - #${env.BUILD_NUMBER}",
+//               body: """
+//                     <p>The build ${buildResult.toLowerCase()}.</p>
+//                     <p>Details:</p>
+//                     <ul>
+//                         <li>Job: ${env.JOB_NAME}</li>
+//                         <li>Build Number: ${env.BUILD_NUMBER}</li>
+//                         <!-- Add more details as needed -->
+//                     </ul>
+//                     <p>Check the build console for more information.</p>
+//                     """,
+//               to: r0583291601@gmail.com'
+// }
+
+// return this
+def sendEmailNotification() {
+    emailext subject: "Build ",
+              body: """
+                    <p>The build w.</p>
+                    <p>Details:</p>
+                    <ul>
+                        <li>Job: ww</li>
+                        <li>Build Number: 12</li>
+                        <!-- Add more details as needed -->
+                    </ul>
+                    <p>Check the build console for more information.</p>
+                    """,
+              to: r0583291601@gmail.com'
+}
+
+return this
+
